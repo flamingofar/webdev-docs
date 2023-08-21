eda807eca6c5a78fbccdec86826cad061b223bd5
Malte Skjoldager <malteskjoldager@gmail.com>
2023-08-21 16:06:04 +0200

    typsescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withNextra = require("nextra")({
 	theme: "nextra-theme-docs",
 	themeConfig: "./theme.config.tsx",
+	resolve: {
+		extensions: ["", ".js", ".jsx", ".ts", ".tsx"],
+	},
 });
 
 module.exports = withNextra();
